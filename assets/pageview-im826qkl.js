f82012edd23e15160dcae75dbbaecc8ab2aa09c8
fanrujiang <1066643487@qq.com>
2024-08-20 10:37:55 +0000

    Update pages to output generated at d9c9a7f

--- assets/pageview-im826qkl.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./app-WQ1PjK-F.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://talk2.fanliu.top/"};const n=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-WQ1PjK-F.js").then(r=>r.a7),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{n as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
