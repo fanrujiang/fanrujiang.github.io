dab449274d2257f5a32b98ab6590640cb7a055e6
fanrujiang <1066643487@qq.com>
2024-08-22 07:56:58 +0000

    Update pages to output generated at 1bd3d5b

--- assets/HeroHitokoto-jjMZ6Zrz.js
+++
@@ -1,8 +0,0 @@
-import{a as _}from"./axios-QLjAsgXu.js";import{i as v,G as g,P as f,j as x}from"./app-RIlIKqyn.js";import{_ as k}from"./plugin-vue_export-helper-x3n3nnut.js";const y=v({__name:"HeroHitokoto",setup(E){const p=o=>{const t=window;clearInterval(t.Timer);const n=document.getElementsByClassName("vp-blog-hero-description");if(n.length>0){const m=n[0];m.id="hotWord"}const d=document.getElementById("hotWord");if(!d)return;d.innerHTML=`
-<div class="word">
-  <div class="left">『</div>
-    <span id="hitokoto_text"></span>
-  <div class="right">』</div>
-</div>
-<div class="author" id="hitokoto_author"> ——「${o.from}」</div>
-  `;const r=document.getElementById("hitokoto_author"),h=document.getElementById("hitokoto_text");if(!h||!r)return;r.style.opacity="0";const c=o.hitokoto.split("");let e=0,s=0,u=60,l=!1,i="";t.Timer=setInterval(()=>{const m=c[e];if(i.length<4?r.style.opacity="0":r.style.opacity="1",s!=0){s--,i.length==0&&s==0&&(clearInterval(t.Timer),a());return}l?i=i.slice(0,e):i+=m,l?e--:e++,e>=c.length&&(l=!0,e=c.length,s=u),e<0&&(l=!1,e=0,s=u),h.innerHTML=i},200)},a=o=>{let t=window.location.pathname;o&&(t=o),t=="/blog/"&&_({method:"get",url:"https://v1.hitokoto.cn",params:{}}).then(n=>{p(n.data)})};return g(()=>{f(()=>{a()}),x().beforeEach(t=>{f(()=>{a(t.fullPath)})})}),()=>{}}}),w=k(y,[["__file","HeroHitokoto.vue"]]);export{w as default};
